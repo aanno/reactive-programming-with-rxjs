442bbad8fcc6b2730020b7ea6f6fdfb3a448d110
Thomas Pasch <thomas.pasch@siteos.de>
2017-05-10 14:42:02 +0200

    es6 changes

--- jspm/src/earthquake/code.ts
+++ jspm/src/earthquake/code.ts
@@ -5,31 +5,30 @@
  * courses, books, articles, and the like. Contact us if you are in doubt.
  * We make no guarantees that this code is fit for any purpose.
  * Visit http://www.pragmaticprogrammer.com/titles/smreactjs for more book information.
-***/
+ ***/
 import Rx from "rx-dom"
 import L from "leaflet"
-import {QUAKE_URL, map} from "./config.ts"
+import {map, QUAKE_URL} from "./config.ts"
 
-var codeLayers = {};
-var quakeLayer = L.layerGroup([]).addTo(map);
-var identity = Rx.helpers.identity; //(1)
+const codeLayers = {};
+const quakeLayer = L.layerGroup([]).addTo(map);
+const identity = Rx.helpers.identity; // (1)
 
 function isHovering(element) {
-  var over = Rx.DOM.mouseover(element).map(identity(true)); //(2)
-  var out = Rx.DOM.mouseout(element).map(identity(false)); //(3)
+  const over = Rx.DOM.mouseover(element).map(identity(true)); // (2)
+  const out = Rx.DOM.mouseout(element).map(identity(false)); // (3)
 
-  return over.merge(out); //(4)
-
+  return over.merge(out); // (4)
 }
 
 function makeRow(props) {
-  var row = document.createElement('tr');
+  const row = document.createElement('tr');
   row.id = props.net + props.code;
 
-  var date = new Date(props.time);
-  var time = date.toString();
-  [props.place, props.mag, time].forEach(function(text) {
-    var cell = document.createElement('td');
+  const date = new Date(props.time);
+  const time = date.toString();
+  [props.place, props.mag, time].forEach(function (text) {
+    const cell = document.createElement('td');
     cell.textContent = text;
     row.appendChild(cell);
   });
@@ -38,63 +37,63 @@ function makeRow(props) {
 }
 
 function initialize() {
-  var quakes = Rx.Observable
+  const quakes = Rx.Observable
     .interval(5000)
-    .flatMap(function() {
-     return Rx.DOM.jsonpRequest({
+    .flatMap(function () {
+      return Rx.DOM.jsonpRequest({
         url: QUAKE_URL,
-        jsonpCallback: 'eqfeed_callback'
+        jsonpCallback: 'eqfeed_callback',
       }).retry(3)
     })
-    .flatMap(function(result) {
+    .flatMap(function (result) {
       return Rx.Observable.from(result.response.features);
     })
-    .distinct(function(quake){
+    .distinct(function (quake) {
       return quake.properties.code
     }).share()
 
-  quakes.subscribe(function(quake) {
-    var coords = quake.geometry.coordinates
-    var size = quake.properties.mag * 10000
+  quakes.subscribe(function (quake) {
+    const coords = quake.geometry.coordinates
+    const size = quake.properties.mag * 10000
 
-    var circle = L.circle([coords[1], coords[0]], size).addTo(map);
+    const circle = L.circle([coords[1], coords[0]], size).addTo(map);
     quakeLayer.addLayer(circle);
     codeLayers[quake.id] = quakeLayer.getLayerId(circle);
   });
 
-  var table = document.getElementById('quakes_info');
+  const table = document.getElementById('quakes_info');
 
   function getRowFromEvent(event) {
     return Rx.Observable
       .fromEvent(table, event)
-      .filter(function(event) { //(1)
-        var el = event.target;
+      .filter(function (event) { // (1)
+        const el = event.target;
         return el.tagName === 'TD' && el.parentNode.id.length;
       })
-      .pluck('target', 'parentNode') //(2)
-      .distinctUntilChanged(); //(3)
+      .pluck('target', 'parentNode') // (2)
+      .distinctUntilChanged(); // (3)
   }
 
   getRowFromEvent('mouseover')
     .pairwise()
-    .subscribe(function(rows) {
-       var prevCircle = quakeLayer.getLayer(codeLayers[rows[0].id]);
-       var currCircle = quakeLayer.getLayer(codeLayers[rows[1].id]);
+    .subscribe(function (rows) {
+      const prevCircle = quakeLayer.getLayer(codeLayers[rows[0].id]);
+      const currCircle = quakeLayer.getLayer(codeLayers[rows[1].id]);
 
-       prevCircle.setStyle({ color: '#0000ff' });
-       currCircle.setStyle({ color: '#ff0000' });
-      });
+      prevCircle.setStyle({color: '#0000ff'});
+      currCircle.setStyle({color: '#ff0000'});
+    });
 
   getRowFromEvent('click')
-    .subscribe(function(row) {
-       var circle = quakeLayer.getLayer(codeLayers[row.id]);
-       map.panTo(circle.getLatLng());
-      });
+    .subscribe(function (row) {
+      var circle = quakeLayer.getLayer(codeLayers[row.id]);
+      map.panTo(circle.getLatLng());
+    });
 
   quakes
     .pluck('properties')
     .map(makeRow)
-    .subscribe(function(row) {
+    .subscribe(function (row) {
       table.appendChild(row);
     });
 }
---
+++ jspm/src/earthquake/config.ts
@@ -0,0 +1,16 @@
+import Rx from "rx-dom"
+import L from "leaflet"
+
+export const QUAKE_URL = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/' +
+  'summary/all_day.geojsonp'
+
+function loadJSONP(url) {
+  var script = document.createElement('script')
+  script.src = url
+
+  var head = document.getElementsByTagName('head')[0]
+  head.appendChild(script)
+}
+
+export const map = L.map('map').setView([33.858631, -118.279602], 7)
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map)
