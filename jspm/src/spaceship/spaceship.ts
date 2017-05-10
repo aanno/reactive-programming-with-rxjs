d91ea1e50b4d319f0db32c0adf242d9490f70022
Thomas Pasch <thomas.pasch@siteos.de>
2017-05-10 15:22:19 +0200

    es6

--- jspm/src/spaceship/spaceship.ts
+++ jspm/src/spaceship/spaceship.ts
@@ -5,220 +5,230 @@
  * courses, books, articles, and the like. Contact us if you are in doubt.
  * We make no guarantees that this code is fit for any purpose.
  * Visit http://www.pragmaticprogrammer.com/titles/smreactjs for more book information.
-***/
+ ***/
 import Rx from "rx"
-import log from "loglevel"
 
-var canvas = document.createElement('canvas');
-var ctx = canvas.getContext("2d");
-document.body.appendChild(canvas);
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+const canvas: HTMLCanvasElement = document.createElement('canvas')
+const ctx: CanvasRenderingContext2D = canvas.getContext("2d")
+document.body.appendChild(canvas)
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
 function isVisible(obj) {
   return obj.x > -40 && obj.x < canvas.width + 40 &&
-    obj.y > -40 && obj.y < canvas.height + 40;
+    obj.y > -40 && obj.y < canvas.height + 40
 }
 
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 function paintStars(stars) {
-  ctx.fillStyle = '#000000';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = '#ffffff';
-  stars.forEach(function(star) {
-    ctx.fillRect(star.x, star.y, star.size, star.size);
-  });
+  ctx.fillStyle = '#000000'
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  ctx.fillStyle = '#ffffff'
+  stars.forEach(function (star) {
+    ctx.fillRect(star.x, star.y, star.size, star.size)
+  })
 }
 
 function gameOver(ship, enemies) {
-  return enemies.some(function(enemy) {
+  return enemies.some(function (enemy) {
     if (collision(ship, enemy)) {
-      return true;
+      return true
     }
 
-    return enemy.shots.some(function(shot) {
-      return collision(ship, shot);
-    });
-  });
+    return enemy.shots.some(function (shot) {
+      return collision(ship, shot)
+    })
+  })
 }
 
 function collision(target1, target2) {
   return (target1.x > target2.x - 20 && target1.x < target2.x + 20) &&
-         (target1.y > target2.y - 20 && target1.y < target2.y + 20);
+    (target1.y > target2.y - 20 && target1.y < target2.y + 20)
 }
 
 function paintScore(score) {
   // console.log("score", score)
-  ctx.fillStyle = '#ffffff';
-  ctx.font = 'bold 26px sans-serif';
-  ctx.fillText('Score: ' + score, 40, 43);
+  ctx.fillStyle = '#ffffff'
+  ctx.font = 'bold 26px sans-serif'
+  ctx.fillText('Score: ' + score, 40, 43)
 }
 
-function drawTriangle(x, y, width, color, direction) {
-  ctx.fillStyle = color;
-  ctx.beginPath();
-  ctx.moveTo(x - width, y);
-  ctx.lineTo(x, direction === 'up' ? y - width : y + width);
-  ctx.lineTo(x + width, y);
-  ctx.lineTo(x - width,y);
-  ctx.fill();
+function drawTriangle(x: number, y: number, width: number, color: string, direction: "up" | "down") {
+  ctx.fillStyle = color
+  ctx.beginPath()
+  ctx.moveTo(x - width, y)
+  ctx.lineTo(x, direction === 'up' ? y - width : y + width)
+  ctx.lineTo(x + width, y)
+  ctx.lineTo(x - width, y)
+  ctx.fill()
 }
 
 function paintSpaceShip(x, y) {
-  drawTriangle(x, y, 20, '#ff0000', 'up');
+  drawTriangle(x, y, 20, '#ff0000', 'up')
 }
 
 function paintEnemies(enemies) {
-  enemies.forEach(function(enemy) {
-    enemy.y += 5;
-    enemy.x += getRandomInt(-15, 15);
+  enemies.forEach(function (enemy) {
+    enemy.y += 5
+    enemy.x += getRandomInt(-15, 15)
 
     if (!enemy.isDead) {
-      drawTriangle(enemy.x, enemy.y, 20, '#00ff00', 'down');
+      drawTriangle(enemy.x, enemy.y, 20, '#00ff00', 'down')
     }
 
-    enemy.shots.forEach(function(shot) {
-      shot.y += SHOOTING_SPEED;
-      drawTriangle(shot.x, shot.y, 5, '#00ffff', 'down');
-    });
-  });
+    enemy.shots.forEach(function (shot) {
+      shot.y += SHOOTING_SPEED
+      drawTriangle(shot.x, shot.y, 5, '#00ffff', 'down')
+    })
+  })
 }
 
-var SHOOTING_SPEED = 15;
-var SCORE_INCREASE= 15;
+const SHOOTING_SPEED = 15
+const SCORE_INCREASE = 15
 function paintHeroShots(heroShots, enemies) {
-  heroShots.forEach(function(shot, i) {
-    try { var enemies_length = enemies.length }
-    catch(err) { var enemies_length = 0 }
-    for (var l=0; l < enemies_length || 0; l++) {
-      var enemy = enemies[l];
+  heroShots.forEach(function (shot, i) {
+    let enemies_length
+    try {
+      enemies_length = enemies.length
+    }
+    catch (err) {
+      enemies_length = 0
+    }
+    for (let l = 0; l < enemies_length || 0; l++) {
+      const enemy = enemies[l]
       if (!enemy.isDead && collision(shot, enemy)) {
-        ScoreSubject.onNext(SCORE_INCREASE);
-        enemy.isDead = true;
-        shot.x = shot.y = -100;
-        break;
+        ScoreSubject.onNext(SCORE_INCREASE)
+        enemy.isDead = true
+        shot.x = shot.y = -100
+        break
       }
     }
 
-    shot.y -= SHOOTING_SPEED;
-    drawTriangle(shot.x, shot.y, 5, '#ffff00', 'up');
-  });
+    shot.y -= SHOOTING_SPEED
+    drawTriangle(shot.x, shot.y, 5, '#ffff00', 'up')
+  })
 }
 
-var SPEED = 40;
-var STAR_NUMBER = 250;
-var StarStream = Rx.Observable.range(1, STAR_NUMBER)
-  .map(function() {
+const SPEED = 40
+const STAR_NUMBER = 250
+const StarStream = Rx.Observable.range(1, STAR_NUMBER)
+  .map(function () {
     return {
       x: parseInt(Math.random() * canvas.width),
       y: parseInt(Math.random() * canvas.height),
-      size: Math.random() * 3 + 1
-    };
+      size: Math.random() * 3 + 1,
+    }
   })
   .toArray()
-  .flatMap(function(starArray) {
-    return Rx.Observable.interval(SPEED).map(function() {
-      starArray.forEach(function(star) {
+  .flatMap(function (starArray) {
+    return Rx.Observable.interval(SPEED).map(function () {
+      starArray.forEach(function (star) {
         if (star.y >= canvas.height) {
-          star.y = 0;
+          star.y = 0
         }
-        star.y += 3;
-      });
-      return starArray;
-    });
-  });
+        star.y += 3
+      })
+      return starArray
+    })
+  })
 
-var HERO_Y = canvas.height - 30;
-var mouseMove = Rx.Observable.fromEvent(canvas, 'mousemove');
-var SpaceShip = mouseMove
-  .map(function(event) { return { x: event.clientX, y: HERO_Y }; })
-  .startWith({ x: canvas.width / 2, y: HERO_Y });
+const HERO_Y = canvas.height - 30
+const mouseMove = Rx.Observable.fromEvent(canvas, 'mousemove')
+const SpaceShip = mouseMove
+  .map(function (event) {
+    return {x: event.clientX, y: HERO_Y}
+  })
+  .startWith({x: canvas.width / 2, y: HERO_Y})
 
 function isVisible(obj) {
   return obj.x > -40 && obj.x < canvas.width + 40 &&
-    obj.y > -40 && obj.y < canvas.height + 40;
+    obj.y > -40 && obj.y < canvas.height + 40
 }
 
-var ENEMY_FREQ = 1500;
-var ENEMY_SHOOTING_FREQ = 750;
-var Enemies = Rx.Observable.interval(ENEMY_FREQ)
-  .scan(function(enemyArray) {
-    var enemy = {
+const ENEMY_FREQ = 1500
+const ENEMY_SHOOTING_FREQ = 750
+const Enemies = Rx.Observable.interval(ENEMY_FREQ)
+  .scan(function (enemyArray) {
+    const enemy = {
       x: parseInt(Math.random() * canvas.width),
       y: -30,
-      shots: []
-    };
+      shots: [],
+    }
 
-    Rx.Observable.interval(ENEMY_SHOOTING_FREQ).subscribe(function() {
+    Rx.Observable.interval(ENEMY_SHOOTING_FREQ).subscribe(function () {
       if (!enemy.isDead) {
-        enemy.shots.push({ x: enemy.x, y: enemy.y });
+        enemy.shots.push({x: enemy.x, y: enemy.y})
       }
-      enemy.shots = enemy.shots.filter(isVisible);
-    });
+      enemy.shots = enemy.shots.filter(isVisible)
+    })
 
-    enemyArray.push(enemy);
+    enemyArray.push(enemy)
     return enemyArray
       .filter(isVisible)
-      .filter(function(enemy) {
-        return !(enemy.isDead && enemy.shots.length === 0);
-      });
-  }, []);
+      .filter(function (enemy) {
+        return !(enemy.isDead && enemy.shots.length === 0)
+      })
+  }, [])
 
-var playerFiring = Rx.Observable
+const playerFiring = Rx.Observable
   .merge(
     Rx.Observable.fromEvent(canvas, 'click'),
     Rx.Observable.fromEvent(canvas, 'keydown')
-      .filter(function(evt) { return evt.keycode === 32; })
+      .filter(function (evt) {
+        return evt.keycode === 32
+      }),
   )
   .sample(200)
-  .timestamp();
+  .timestamp()
 
-var HeroShots = Rx.Observable
+const HeroShots = Rx.Observable
   .combineLatest(
     playerFiring,
     SpaceShip,
-    function(shotEvents, spaceShip) {
+    function (shotEvents, spaceShip) {
       return {
         timestamp: shotEvents.timestamp,
-        x: spaceShip.x
-      };
+        x: spaceShip.x,
+      }
     })
-  .distinctUntilChanged(function(shot) { return shot.timestamp; })
-  .scan(function(shotArray, shot) {
-    shotArray.push({ x:shot.x, y: HERO_Y });
-    return shotArray;
-  }, []);
+  .distinctUntilChanged(function (shot) {
+    return shot.timestamp
+  })
+  .scan(function (shotArray, shot) {
+    shotArray.push({x: shot.x, y: HERO_Y})
+    return shotArray
+  }, [])
 
-var ScoreSubject = new Rx.BehaviorSubject(0);
-var score = ScoreSubject.scan(function (prev, cur) {
-  return prev + cur;
-}, 0).concat(Rx.Observable.return(0));
+const ScoreSubject = new Rx.BehaviorSubject(0)
+const score = ScoreSubject.scan(function (prev, cur) {
+  return prev + cur
+}, 0).concat(Rx.Observable.return(0))
 
 function renderScene(actors) {
-  paintStars(actors.stars);
-  paintSpaceShip(actors.spaceship.x, actors.spaceship.y);
-  paintEnemies(actors.enemies);
-  paintHeroShots(actors.heroShots, actors.enemies);
-  paintScore(actors.score);
+  paintStars(actors.stars)
+  paintSpaceShip(actors.spaceship.x, actors.spaceship.y)
+  paintEnemies(actors.enemies)
+  paintHeroShots(actors.heroShots, actors.enemies)
+  paintScore(actors.score)
 }
 
 Rx.Observable.combineLatest(
-    StarStream, SpaceShip, Enemies, HeroShots, score,
-    function(stars, spaceship, enemies, heroShots) {
-      // console.log("score", score)
-      return {
-        stars: stars,
-        spaceship: spaceship,
-        enemies: enemies,
-        heroShots: heroShots,
-        score: score,
-      };
-    })
-  .sample(SPEED)
-  .takeWhile(function(actors) {
-    return gameOver(actors.spaceship, actors.enemies) === false;
+  StarStream, SpaceShip, Enemies, HeroShots, score,
+  function (stars, spaceship, enemies, heroShots) {
+    // console.log("score", score)
+    return {
+      stars: stars,
+      spaceship: spaceship,
+      enemies: enemies,
+      heroShots: heroShots,
+      score: score,
+    }
   })
-  .subscribe(renderScene);
+  .sample(SPEED)
+  .takeWhile(function (actors) {
+    return gameOver(actors.spaceship, actors.enemies) === false
+  })
+  .subscribe(renderScene)
